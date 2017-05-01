9da848b6e153c0f8141aef48950ed49122c605e6
gary sun <garysun2@hotmail.com>
2017-05-01 00:45:15 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(ara)
+    puts " #{ara[0]} | #{ara[1]} | #{ara[2]} "
+    puts "-"*11
+    puts " #{ara[3]} | #{ara[4]} | #{ara[5]} "
+    puts "-"*11
+    puts " #{ara[6]} | #{ara[7]} | #{ara[8]} "
+end
